27f91c9edabf4252b875ae1311c629cc9d4105db
Cristhian Bravo <green2cerberuz@gmail.com>
2021-05-16 06:41:22 -0500

    Add login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,21 @@
 Rails.application.routes.draw do
   # Get /about
   get "about_us", to: "about#index", as: :about
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
   root "main#index"
 
 end
